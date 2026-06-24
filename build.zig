e3365b0c36e52e731cd7911ab853b96cf7f70723
Caleb Cole <calebrylancole@gmail.com>
2026-06-24 13:05:17 -0400

    feat: Working build with Vulkan & GLFW
    Uses packages vulkan-zig and zglfw for zig bindings to Vulkan and GLFW respectively.

--- build.zig
+++ build.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    const exe = b.addExecutable(.{
+        .name = "Cubix",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.graph.host,
+            .link_libc = true,
+        }),
+    });
+
+    const vulkan = b.dependency("vulkan", .{
+        .registry = b.path("vendor/vk.xml"),
+    }).module("vulkan-zig");
+    exe.root_module.addImport("vulkan", vulkan);
+
+    const zglfw = b.dependency("zglfw", .{});
+    exe.root_module.addImport("zglfw", zglfw.module("root"));
+
+    exe.root_module.linkLibrary(zglfw.artifact("glfw"));
+
+    b.installArtifact(exe);
 }
