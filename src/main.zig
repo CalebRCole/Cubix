c4564975bf8b403e54c6a8b28e9a3a72f5562ede
Caleb Cole <calebrylancole@gmail.com>
2026-06-25 08:55:24 -0400

    feat: Add surface creation

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
+const glfw = @import("zglfw");
+const vk = @import("vulkan-zig");
+
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+    try glfw.init();
+    defer glfw.terminate();
+
+    const window = try glfw.createWindow(600, 600, "Cubix", null, null);
+    defer glfw.destroyWindow(window);
+    glfw.makeContextCurrent(window);
+
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+
+        window.swapBuffers();
+    }
 }
